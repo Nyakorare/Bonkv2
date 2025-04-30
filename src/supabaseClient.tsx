b1557fa02960634226f16b6c7e27b40c9fee784b
Nyakorare <g1galba042804@gmail.com>
2025-04-30 16:25:47 +0800

    commentss

--- src/supabaseClient.tsx
+++ src/supabaseClient.tsx
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
+//brute force muna tangena katamad mag tago pa ako lang naman may access HAHHAHA
 const supabaseUrl = 'https://bqkwoinphwfxinmsyfnw.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxa3dvaW5waHdmeGlubXN5Zm53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMDIzOTYsImV4cCI6MjA1OTg3ODM5Nn0.wXsaIBPQFz0hcoavMNpkhFj8hXr_Z7pRdstgnbsPdLE'
 
